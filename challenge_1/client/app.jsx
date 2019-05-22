4d8b294d6128b698461720b63b2aa47dcd227921
EricZH <letItBezhumphries>
2019-05-21 23:54:11 -0700

    json-server is serving events on loading App

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,2 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    }
+  }
+  
+  componentWillMount() {
+    const app = this;
+    axios.get('http://localhost:3000/events')
+    .then(response => {
+      console.log('this should be the events', response.data);
+      this.setState({
+        events: response.data
+      });
+    })
+    .catch(err => {
+      console.log('there was a problem hitting the json server', err);
+    })
+  }
 
 
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const SRC_DIR = path.join(__dirname, './client/index.jsx');
+const DIST_DIR = path.join(__dirname, './public/dist');
+
+module.exports = {
+  entry: SRC_DIR,
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      // include: SRC_DIR, 
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    },
+    { 
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }, 
+    {
+      test: /\.json$/,
+      use: ['json-loader']
+    }]
+  }
+}
